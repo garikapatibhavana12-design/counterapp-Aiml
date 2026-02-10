63117018bb98bc558a3528741a1c10d67acdc18b
garikapatibhavana12-design <garikapatibhavana12@gmail.com>
2026-02-10 10:18:58 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <h1>
+        Counter</h1>
+
+      <h2>{count}</h2>
+
+      <div className="buttons">
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={() => setCount(count - 1)}>-</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
